e185b5c458918bb571ada6d12e2455a98acc323a
msftguy <msft.guy@gmail.com>
2011-09-03 00:52:04 +0400

    Reorg; added IDA automation; moved scripts to https://github.com/msftguy/ios-jb-tools/blob/master/tools_src/idapy/

---
+++ ref_table.h
@@ -0,0 +1,11 @@
+#pragma once
+REF_ENTRY ref_table[] = {
+// iOS 5.0 Beta 7
+// generated automatically with commcenter.py, IDA and IdaPython
+	// +xsimstate=1
+	{0x12049e, 0x358c2, 0x35865},
+	// Sending internal notification %s
+	{0x12866e, 0x68790, 0x68741},
+	// activation ticket accepted... drive thru
+	{0x120416, 0x34c3e, 0x34acd},
+};
